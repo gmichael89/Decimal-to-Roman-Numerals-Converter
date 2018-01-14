20fa6af688278dc018123a7af339cbd247384d17
gmichael89 <g.michael0704@gmail.com>
2018-01-14 14:48:31 +0000

    Updated the form with a validation message.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import generate from './generate'
+const AppConfig = require('../app.config.js');
 
 document.getElementById('Submit').addEventListener('click', () => {
 
     const inputValue = document.getElementById('NumberInput').value;
+    let result = generate(inputValue);
 
-    document.getElementById('Result').value = generate(inputValue);
+    if (!result) {
+        result = `Input invalid. Ensure that you are passing in a number and it
+                    is within the range ${AppConfig.NumberRange.Min} and
+                    ${AppConfig.NumberRange.Max}.`
+    }
+
+    document.getElementById('Result').innerHTML = result;
 
 })
